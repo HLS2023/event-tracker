1f238aa85c5335b4e0632b975d979856c7cb150b
Krystal Phu <krystalphu@college.harvard.edu>
2017-12-02 21:23:07 +0000

    changed index

--- index.js
+++ index.js
@@ -60,17 +60,48 @@ app.post('/webhook', (req, res) => {
 
 });
 
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
+function processPostback(event) {
+  var senderId = event.sender.id;
+  var payload = event.postback.payload;
 
+  if (payload === "Greeting") {
+    // Get user's first name from the User Profile API
+    // and include it in the greeting
+    request({
+      url: "https://graph.facebook.com/v2.6/" + senderId,
+      qs: {
+        access_token: process.env.PAGE_ACCESS_TOKEN,
+        fields: "first_name"
+      },
+      method: "GET"
+    }, function(error, response, body) {
+      var greeting = "";
+      if (error) {
+        console.log("Error getting user's name: " +  error);
+      } else {
+        var bodyObj = JSON.parse(body);
+        name = bodyObj.first_name;
+        greeting = "Hi " + name + ". ";
+      }
+      var message = greeting + "Hi. I am the Harvard Event Tracker!";
+      sendMessage(senderId, {text: message});
+    });
+  }
 }
 
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-
+// sends message to user
+function sendMessage(sender_psid, message) {
+  request({
+    url: "https://graph.facebook.com/v2.6/me/messages",
+    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+    method: "POST",
+    json: {
+      recipient: {id: sender_psid},
+      message: message,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log("Error sending message: " + response.error);
+    }
+  });
 }
