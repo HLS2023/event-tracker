b98b2bddfc8986d7d6a87f3f5fe2a7739c9943f3
Krystal Phu <krystalphu@college.harvard.edu>
2017-12-05 01:36:56 +0000

    omg

--- index.js
+++ index.js
@@ -18,12 +18,14 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 'use strict';
 
+
 // Imports dependencies and set up http server
 const
   request = require('request'),
   express = require('express'),
   body_parser = require('body-parser'),
   app = express().use(body_parser.json()); // creates express http server
+
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
@@ -112,40 +114,39 @@ function handleMessage(sender_psid, received_message) {
   let response;
 
   // Checks if the message contains text
-  // if (received_message.text) {
+  if (received_message.text) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
-    // response = {
-      // "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-    // };
-  // }
-  if (received_message.text) {
-    // Get the URL of the message attachment
-    // let attachment_url = received_message.attachments[0].payload.url;
     response = {
-      //"attachment": {
-        // "type": "template",
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    };
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
         "payload": {
           "template_type": "generic",
           "elements": [{
-            "title": "What date are you looking for?",
+            "title": "Is this the right picture?",
             "subtitle": "Tap a button to answer.",
-            // "image_url": attachment_url,
+            "image_url": attachment_url,
             "buttons": [
               {
                 "type": "postback",
-                "title": "Friday",
-                "payload": "friday",
+                "title": "Yes!",
+                "payload": "yes",
               },
               {
                 "type": "postback",
-                "title": "Saturday!",
-                "payload": "saturday",
+                "title": "No!",
+                "payload": "no",
               }
             ],
           }]
         }
-      // }
+      }
     };
   }
 
@@ -161,10 +162,10 @@ function handlePostback(sender_psid, received_postback) {
   let payload = received_postback.payload;
 
   // Set the response based on the postback payload
-  if (payload === 'friday') {
-    response = { "text": "Here's what's happening Friday!" };
-  } else if (payload === 'saturday') {
-    response = { "text": "Here's what's happening Saturday!" };
+  if (payload === 'yes') {
+    response = { "text": "Thanks!" };
+  } else if (payload === 'no') {
+    response = { "text": "Oops, try sending another image." };
   }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
@@ -193,4 +194,160 @@ function callSendAPI(sender_psid, response) {
       console.error("Unable to send message:" + err);
     }
   });
+}
+
+
+// Citations:
+// 1. Google Calendar API, https://developers.google.com/google-apps/calendar/quickstart/js
+
+let fs = require('fs');
+let readline = require('readline');
+let google = require('googleapis');
+let googleAuth = require('google-auth-library');
+let housecalendarids = ['0spevsj3f418i587nsipqdoppk@group.calendar.google.com',
+                        'kirkland.website@gmail.com',
+                        'mather.calendar@gmail.com',
+                        'adamshousetutors@gmail.com'];
+let allstarts = [];
+let allends = [];
+let allsummaries = [];
+
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/calendar-nodejs-quickstart.json
+let SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+let TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+let TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
+
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+  if (err) {
+    console.log('Error loading client secret file: ' + err);
+    return;
+  }
+  // Authorize a client with the loaded credentials, then call the
+  // Google Calendar API.
+  authorize(JSON.parse(content), listEvents);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  let clientSecret = credentials.installed.client_secret;
+  let clientId = credentials.installed.client_id;
+  let redirectUrl = credentials.installed.redirect_uris[0];
+  let auth = new googleAuth();
+  let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+    }
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+  let authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token)
+{
+  try
+  {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST')
+    {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
+}
+
+/**
+ * Lists the next 4 events on each of the four houses' calendars.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listEvents(auth)
+{
+  let calendar = google.calendar('v3');
+  for (let i = 0; i < housecalendarids.length; i++)
+  {
+    calendar.events.list(
+    {
+      auth: auth,
+      calendarId: housecalendarids[i],
+      timeMin: (new Date()).toISOString(),
+      maxResults: 4,
+      singleEvents: true,
+      orderBy: 'startTime'
+    },
+    function(err, response)
+    {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        return;
+      }
+      let events = response.items;
+      if (events.length == 0)
+      {
+        console.log('No upcoming events in calendar.');
+      }
+      else
+      {
+        for (let j = 0; j < events.length; j++) {
+          let event = events[j];
+          let start = event.start.dateTime || event.start.date;
+          let end = event.end.dateTime || event.end.date;
+          allstarts.push(start);
+          allends.push(end);
+          allsummaries.push(event.summary);
+        }
+      }
+    });
+  }
 }
