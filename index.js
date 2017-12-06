a32d7204e5fa3b460a3e17a077b1dabb46dcb3c5
Krystal Phu <krystalphu@college.harvard.edu>
2017-12-06 17:02:29 +0000

    tremendous

--- index.js
+++ index.js
@@ -1,49 +1,39 @@
+/**
+ * Copyright 2017-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ *
+ * Starter Project for Messenger Platform Quick Start Tutorial
+ *
+ * Use this project as the starting point for following the
+ * Messenger Platform quick start tutorial.
+ *
+ * https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start/
+ *
+ */
+
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 'use strict';
 
+
+// Imports dependencies and set up http server
 const
-  // Express HTTP
   request = require('request'),
   express = require('express'),
   body_parser = require('body-parser'),
-  app = express().use(body_parser.json());
+  app = express().use(body_parser.json()); // creates express http server
+
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
+
 // Server index page
 app.get("/", function (req, res) {
   res.send("Deployed!");
-});
-
-// Accepts GET requests at the /webhook endpoint
-app.get('/webhook', (req, res) => {
-
-  /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = "VERIFICATION_TOKEN";
-
-  // Parse params from the webhook verification request
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-
-  // Check if a token and mode were sent
-  if (mode && token) {
-
-    // Check the mode and token sent are correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-      // Respond with 200 OK and challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    }
-
-    else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
-  }
 });
 
 
@@ -87,6 +77,37 @@ app.post('/webhook', (req, res) => {
   }
 
 });
+
+
+// Accepts GET requests at the /webhook endpoint
+app.get('/webhook', (req, res) => {
+
+  /** UPDATE YOUR VERIFY TOKEN **/
+  const VERIFY_TOKEN = "VERIFICATION_TOKEN";
+
+  // Parse params from the webhook verification request
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  // Check if a token and mode were sent
+  if (mode && token) {
+
+    // Check the mode and token sent are correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+      // Respond with 200 OK and challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    }
+
+    else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
+
 
 function handleMessage(sender_psid, received_message) {
 	let response;
@@ -163,7 +184,7 @@ function handlePostback(sender_psid, received_postback) {
 
   // Set the response based on the postback payload
   if (payload === 'Friday') {
-    response = { "text": allsummaries[0] };r
+    response = { "text": "This is what is happening Friday!" };
   } else if (payload === 'Saturday') {
     response = { "text": "This is what is happening Saturday." };
   }
