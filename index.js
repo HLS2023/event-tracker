b8ae2eb9a3027b68deb9fcd0d87f66b2966e4e64
Krystal Phu <krystalphu@college.harvard.edu>
2017-12-06 15:59:32 +0000

    restructuring

--- index.js
+++ index.js
@@ -38,137 +38,13 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
   authorize(JSON.parse(content), listEvents);
 });
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  let clientSecret = credentials.installed.client_secret;
-  let clientId = credentials.installed.client_id;
-  let redirectUrl = credentials.installed.redirect_uris[0];
-  let auth = new googleAuth();
-  let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, callback);
-    } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client);
-    }
-  });
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
-function getNewToken(oauth2Client, callback) {
-  let authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-  let rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
-      }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
-    });
-  });
-}
-
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
-function storeToken(token)
-{
-  try
-  {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST')
-    {
-      throw err;
-    }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-  console.log('Token stored to ' + TOKEN_PATH);
-}
-
-/**
- * Lists the next 4 events on each of the four houses' calendars.
- *
- * @param {google.auth.OAuth2} auth, an authorized OAuth2 client.
- */
-function listEvents(auth)
-{
-  let calendar = google.calendar('v3');
-  for (let i = 0; i < housecalendarids.length; i++)
-  {
-    calendar.events.list(
-    {
-      auth: auth,
-      calendarId: housecalendarids[i],
-      timeMin: (new Date()).toISOString(),
-      maxResults: 4,
-      singleEvents: true,
-      orderBy: 'startTime'
-    },
-    function(err, response)
-    {
-      if (err) {
-        console.log('The API returned an error: ' + err);
-        return;
-      }
-      let events = response.items;
-      if (events.length == 0)
-      {
-        console.log('No upcoming events in calendar.');
-      }
-      else
-      {
-        for (let j = 0; j < events.length; j++) {
-          let event = events[j];
-          let start = event.start.dateTime || event.start.date;
-          let end = event.end.dateTime || event.end.date;
-          allevents.push(event);
-          allstarts.push(start);
-          allends.push(end);
-          allsummaries.push(event.summary);
-        }
-      }
-    });
-  }
-}
-
-/* Facebook API */
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 'use strict';
 
 
-// Imports dependencies and set up http server
+// Imports dependencies and set sup http server
 const
   request = require('request'),
   express = require('express'),
@@ -333,7 +209,7 @@ function handlePostback(sender_psid, received_postback) {
 
   // Set the response based on the postback payload
   if (payload === 'Friday') {
-    response = { "text": allsummaries[0] };
+    response = { "text": allsummaries[0] };r
   } else if (payload === 'Saturday') {
     response = { "text": "This is what is happening Saturday." };
   }
@@ -367,3 +243,118 @@ function callSendAPI(sender_psid, response) {
   });
 }
 
+function authorize(credentials, callback) {
+  let clientSecret = credentials.installed.client_secret;
+  let clientId = credentials.installed.client_id;
+  let redirectUrl = credentials.installed.redirect_uris[0];
+  let auth = new googleAuth();
+  let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+    }
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+  let authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token)
+{
+  try
+  {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST')
+    {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
+}
+
+/**
+ * Lists the next 4 events on each of the four houses' calendars.
+ *
+ * @param {google.auth.OAuth2} auth, an authorized OAuth2 client.
+ */
+function listEvents(auth)
+{
+  let calendar = google.calendar('v3');
+  for (let i = 0; i < housecalendarids.length; i++)
+  {
+    calendar.events.list(
+    {
+      auth: auth,
+      calendarId: housecalendarids[i],
+      timeMin: (new Date()).toISOString(),
+      maxResults: 4,
+      singleEvents: true,
+      orderBy: 'startTime'
+    },
+    function(err, response)
+    {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        return;
+      }
+      let events = response.items;
+      if (events.length == 0)
+      {
+        console.log('No upcoming events in calendar.');
+      }
+      else
+      {
+        for (let j = 0; j < events.length; j++) {
+          let event = events[j];
+          let start = event.start.dateTime || event.start.date;
+          let end = event.end.dateTime || event.end.date;
+          allevents.push(event);
+          allstarts.push(start);
+          allends.push(end);
+          allsummaries.push(event.summary);
+        }
+      }
+    });
+  }
+}
