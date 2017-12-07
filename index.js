5fdde42299c717f511fd8f453645d8afbbf84d79
Krystal Phu <krystalphu@college.harvard.edu>
2017-12-07 00:38:48 +0000

    is this it?

--- index.js
+++ index.js
@@ -228,16 +228,3 @@ function callSendAPI(sender_psid, response) {
   });
 }
 
-
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", "cabcaf.json", true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    };
-    rawFile.send(null);
-}
-
